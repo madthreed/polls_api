331d45a7ea456772968057404c6e527d7ee0e49a
madthreed <madthreed@gmail.com>
2022-03-16 13:05:18 +0300

    initial

---
+++ src/main/java/com/madthreed/polls_api/PollsApiApplication.java
@@ -0,0 +1,13 @@
+package com.madthreed.polls_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/madthreed/polls_api/PollsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madthreed.polls_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
