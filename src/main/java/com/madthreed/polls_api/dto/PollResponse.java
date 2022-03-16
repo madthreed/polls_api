6fdc2487446a095635432448da6dd78fa146b707
madthreed <madthreed@gmail.com>
2022-03-16 17:36:09 +0300

    create dto mapper and DtoMapperTest

--- src/main/java/com/madthreed/polls_api/dto/PollResponse.java
+++ src/main/java/com/madthreed/polls_api/dto/PollResponse.java
@@ -1,11 +1,13 @@
 package com.madthreed.polls_api.dto;
 
 import lombok.Builder;
+import lombok.Data;
 
 import java.util.Date;
 import java.util.List;
 
 @Builder
+@Data
 public class PollResponse {
 
     private Long id;
--- src/main/java/com/madthreed/polls_api/dto/QuestionResponse.java
+++ src/main/java/com/madthreed/polls_api/dto/QuestionResponse.java
@@ -2,10 +2,12 @@ package com.madthreed.polls_api.dto;
 
 import com.madthreed.polls_api.model.QuestionType;
 import lombok.Builder;
+import lombok.Data;
 
 import java.util.List;
 
 @Builder
+@Data
 public class QuestionResponse {
 
     private Long id;
